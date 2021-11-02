2447bcbd121ea968acb16fd38675ab12f5005a05
Richard Kendall <richard.kendall@gmail.com>
2021-11-02 13:48:05 +1100

    adding s3 upload

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# get commit id
 COMMIT=`git log --pretty=format:'%h' -n 1`
 
+# create and clean output directories
 mkdir -p {out/resources,out/matches}
+rm -f out/resources/*
+rm -f out/matches/*
 
+# download the tool
+
+
+# for each app, run the tool
 for FILE in apps/*; do
   arrFILE=(${FILE//\// })
   arrFILENAME=(${arrFILE[1]//\./ })
@@ -11,5 +19,9 @@ for FILE in apps/*; do
 
   ./design-as-code -app=$FILE -patternlib=patterns/vmc.hcl -mode=describe -json=out/resources/$COMMIT-$APP_NAME-describe.json
   ./design-as-code -app=$FILE -patternlib=patterns/vmc.hcl -mode=match -json=out/matches/$COMMIT-$APP_NAME-matches.json
-
 done
+
+# now upload to S3
+aws s3 sync out/resources/ s3://$BUCKET/resources/
+aws s3 sync out/matches/ s3://$BUCKET/matches/
+
